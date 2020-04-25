2b48e8070805d926d378dda474f74b45ccf379b4
Qian_ying <1119536376@qq.com>
2020-04-25 22:55:26 +0800

    new change

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("蜜汁游戏~");
+        Scanner a = new Scanner(System.in);
+        System.out.print("Tell me your name: ");
+        String x = a.nextLine();
+        System.out.print("输入一个数： ");
+        int c = a.nextInt();
+        System.out.println("さあぁ、始めるよ！");
+        if (c >= 0) {
+            System.out.println("I win!");
+        } else {
+            System.out.println("You win!");
+        }
+        if (c <= -1) {
+            System.out.println("Oh,that was Interesting");
+        } else {
+            System.out.println("It's boring");
+        }
+        System.out.println("Name: ");
+        System.out.println(x);
+    }
+}
