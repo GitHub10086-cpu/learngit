b6823c23b2601c6c2762a5130624fbafdaa1a232
Qian_ying <1119536376@qq.com>
2020-04-26 22:11:54 +0800

    年龄判断

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
-a = input()
-int x = a
-x = x + 2
-print(x)
-print('hello,world!')
+x = input('age:')
+age = int(x)
+if age >= 18:
+    print('cz')
+elif age >= 6:
+    print('pa')
+else:
+    print('pa!')
