b1efcc339fd78701be84bb6feaeb32643e2386a3
Qian_ying <1119536376@qq.com>
2020-04-27 15:04:37 +0800

    遍历数组

---
+++ Zian/src/com/company/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] ns = { 1, 4, 9, 16, 25 };
+        for (int i=0; i<ns.length; i++) {
+            int n = ns[i];
+            System.out.println(n);
+        }
+    }
+}
